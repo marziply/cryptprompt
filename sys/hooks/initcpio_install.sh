72fe5445ab8dc64edc11ffec59ff599cdb98e483
marziply <contact@marziply.dev>
2022-11-17 22:47:54 +0000

    Filled out initcpio hook scripts

--- sys/hooks/initcpio_install.sh
+++ sys/hooks/initcpio_install.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+build() {
+  add_dir /dev/pts
+
+	add_file /usr/lib/udev/rules.d/70-uaccess.rules
+	add_file /usr/lib/udev/rules.d/71-seat.rules
+
+	add_file /etc/passwd
+	add_file /etc/nsswitch.conf
+	add_binary "$(readlink -e /lib/libnss_files.so.2)"
+	add_file /lib/libnss_files.so.2
+
+	add_runscript
+}
